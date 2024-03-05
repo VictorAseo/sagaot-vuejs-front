86aa4f9960f6eac753d69109d714b6a85c4ba9f7
Victor Aseo <>
2024-03-05 13:17:01 -0300

    Incomplete routes creation

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,18 +1,54 @@
 import { createRouter, createWebHistory, type RouteRecordRaw } from "vue-router";
-import LoginView from "@/views/login/LoginView.vue";
+import SignInView from "@/views/SignIn/SignInView.vue";
+import SignUpView from "@/views/SignUp/SignUpView.vue";
 import HomeView from "@/views/home/HomeView.vue";
 
 
 const routes: Array<RouteRecordRaw> = [
-  {
-    path: "/",
+  { //Dashboard screen
+    path: "/Dashboard_S",
     name: "HomeView",
     component: HomeView,
   },
-  {
-    path:"/login",
-    name:"LoginView",
-    component: LoginView
+  { // Sign in screen
+    path:"/SignInView",
+    name:"SignInView",
+    component: SignInView
+  },
+  { // Sign Up screen
+    path:"/SignUpView",
+    name:"SignUpView",
+    component: SignUpView
+  },
+  { // Professor list screen to student category
+    path:"/ProfessorsListView_S",
+    name:"ProfessorsListView",
+    component: ProfessorsListView
+  },
+  { // GPA Screen
+    path:"/GPAView",
+    name:"GPAView",
+    component: GPAView
+  },
+  { // Message Screen
+    path:"/MessagesView",
+    name:"MessagesView",
+    component: MessagesView
+  },
+  { // Documents/Reports screen
+    path:"/DocumentsView",
+    name:"DocumentsView",
+    component: DocumentsView
+  },
+  { // Students list screen to professor category
+    path:"/StudentsListView_P",
+    name:"StudentsListView",
+    component: StudentsListView
+  },
+  { // Settings screen
+    path:"/SettingsView",
+    name:"SettingsView",
+    component: SettingsView
   }
 ];
 
