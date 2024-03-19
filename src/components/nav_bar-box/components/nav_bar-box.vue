bd3e98327e8f0e975f586f2b67c12e9a0008d042
Victor Aseo <>
2024-03-18 22:59:15 -0300

    currently displayed on screen DashboardView.

---
+++ src/components/nav_bar-box/components/nav_bar-box.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="nav_bar-box__container">
+    <slot></slot>
+  </div>
+</template>
+<script setup lang="ts">
+
+</script>
+<style scoped lang="scss">
+
+  .nav_bar-box__container{
+    height: 539px;
+    width: 158px;
+    background: #ffffff;
+    border-radius: 6px;
+    padding: 18px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin-right: 87px;
+  }
+
+</style>
